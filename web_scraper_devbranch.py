279113a7d769d615c2e1f4980f35e6f90a3e332d
James Kunstle <jkunstle@bu.edu>
2020-05-28 00:00:53 -0600

    adds section, lecturer, location, and schedule now

--- web_scraper_devbranch.py
+++ web_scraper_devbranch.py
@@ -17,6 +17,9 @@ Global Scope: Variables and Report Strings.
 """
 cs_catalog_pages_url = "https://www.bu.edu/academics/cas/courses/computer-science/"
 
+#stem that will be used for the other pages after getting the general course data.
+academics_url_stem = "https://www.bu.edu"
+
 RESPONSE_OKAY_TOKEN = 200
 RESPONSE_NOTFOUND_TOKEN = 404
 SLEEP_TIME = 0.5 #500ms
@@ -29,11 +32,11 @@ FAILED_MESSAGE = "Status: Failed, unspecified code"
 Functions
 """
 
-def print_course_data(input_url, low_bound, high_bound):
+def print_general_course_data(input_url, low_bound, high_bound):
     pages = []
     for i in range(low_bound, high_bound + 1):
         local_url = input_url + str(i)
-        pages.append(get_course_data(local_url))
+        pages.append(get_general_course_data(local_url))
         print("Finshed page: "+ str(i))
         t.sleep(SLEEP_TIME)
 
@@ -44,14 +47,91 @@ def print_course_data(input_url, low_bound, high_bound):
             print("Prereqs:         " + str(course[2]))
             print("")
 
-def get_course_data(input_url):
-    req = requests.get(input_url)
+def print_general_and_schedule_data(input_url, stem_url, low_bound, high_bound):
+        pages = []
+        schedules = []
+        for i in range(low_bound, high_bound + 1):
+            local_url = input_url + str(i) #get url of website page
+            general_course_data = get_general_course_data(local_url) #get the names of the courses, url-end, and prereqs
 
+            pages.append(general_course_data) #add ^^ to the pages list
+            schedules.append(get_professor_and_schedule_data(stem_url, general_course_data)) #add schedule to pages list
+            print("Finshed page: "+ str(i))
+            t.sleep(SLEEP_TIME)
+
+
+        for page, schedule in zip(pages, schedules):
+            for course, schedule_data in zip(page, schedule):
+                print("Course Title:            " + str(course[0]))
+                print("Link:                    " + str(course[1]))
+                print("Prereqs:                 " + str(course[2]))
+                print("Schedule info:\n")
+
+                for entry in schedule_data:
+                     print("")
+                     print("Semester:       " + str(entry[0]))
+
+                     info = str(entry[1]).split('\n')
+                     possible_categories = ["Section", "Instructor", "Location", "Schedule", "Notes"]
+                     for info_section in zip(possible_categories, info[1:]):
+                         print(info_section[0] + ": " + info_section[1])
+
+
+                print("")
+                print("---------------------------------------------------------")
+
+
+def collate_general_and_schedule(input_url, stem_url):
+    print( get_professor_and_schedule_data(stem_url, get_general_course_data(input_url)))
+
+
+def get_professor_and_schedule_data(input_url, course_objects):
+    schedule_objects = []
+    for course in course_objects:
+        t.sleep(SLEEP_TIME)
+        #use the URL for each individual course to access the page.
+        req = requests.get(input_url + course[1])
+
+        course_schedule = []
+
+        if(req.status_code == RESPONSE_OKAY_TOKEN):
+            #parse the HTML from the URL into a soup object.
+            soup = BeautifulSoup(req.content, "html.parser")
+            #based on the structure, we need this object.
+            whole_schedule = soup.find("div", {"class":"cf-course"})
+
+            semesters = whole_schedule.find_all("h4")
+            course_details = whole_schedule.find_all("table")
+
+            for semester, detail in zip(semesters, course_details):
+                sem = semester.find("strong")
+                det = detail.find_all("tr")[1]
+                course_schedule.append([sem.text, det.text])
+
+            schedule_objects.append(course_schedule)
+            print(COMPLETE_MESSAGE)
+        elif(req.status_code == RESPONSE_NOTFOUND_TOKEN):
+            print(NOTFOUND_MESSAGE)
+        else:
+            print(FAILED_MESSAGE)
+
+
+    return schedule_objects
+
+
+#returns an array of courses that include (name, url_postfix, prereqs)
+def get_general_course_data(input_url):
+    #get general data from the URL that is given
+    req = requests.get(input_url)
+    course_objects = []
+
+    #perform necessary error checking
     if(req.status_code == RESPONSE_OKAY_TOKEN):
-        soup = BeautifulSoup(req.content, "html.parser") #conversion from req into text for parsing
+        #parse the HTML from the URL into a soup object.
+        soup = BeautifulSoup(req.content, "html.parser")
+        #based on the structure, we need this object.
         course_feed = soup.find("ul", {"class": "course-feed"})
         feed_entries = course_feed.find_all("li")
-        course_objects = []
         for entry in feed_entries:
             link = entry.find("a")
             name = entry.find("strong")
@@ -61,11 +141,18 @@ def get_course_data(input_url):
                     course_objects.append([name.text, link.attrs["href"], prereq.text])
                 else:
                     course_objects.append([name.text, link.attrs["href"], "No Prerequisites"])
-        return course_objects
         print(COMPLETE_MESSAGE)
     elif(req.status_code == RESPONSE_NOTFOUND_TOKEN):
         print(NOTFOUND_MESSAGE)
     else:
         print(FAILED_MESSAGE)
 
-print_course_data(cs_catalog_pages_url, 1, 3)
+    return course_objects
+
+
+"""
+Calling Functions
+"""
+#print_general_course_data(cs_catalog_pages_url, 1, 3)
+#collate_general_and_schedule(cs_catalog_pages_url, academics_url_stem)
+print_general_and_schedule_data(cs_catalog_pages_url, academics_url_stem, 1, 1)
